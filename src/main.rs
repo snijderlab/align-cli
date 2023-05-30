0caf2bfed683e8efa03f96f0c2b7836475eddf7c
Douwe Schulte <d.schulte@uu.nl>
2023-05-30 14:05:58 +0200

    Added better result display and mode selection

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,96 @@ struct Args {
     /// Second sequence
     #[arg()]
     y: String,
+
+    /// Use global alignment, default
+    #[arg(short, long)]
+    global: bool,
+
+    /// Use semi-global alignment
+    #[arg(short, long)]
+    semi_global: bool,
+
+    /// Use local alignment
+    #[arg(short, long)]
+    local: bool,
 }
 
 fn main() {
     let args = Args::parse();
+    if args.global & args.semi_global || args.global && args.local || args.semi_global && args.local
+    {
+        panic!("Cannot have multiple alignment types at the same time")
+    }
     if args.x.contains(',') {
         for (x, y) in args.x.split(',').zip(args.y.split(',')) {
-            align(x.as_bytes(), y.as_bytes());
+            align(&args, x.as_bytes(), y.as_bytes());
         }
     } else {
-        align(args.x.as_bytes(), args.y.as_bytes());
+        align(&args, args.x.as_bytes(), args.y.as_bytes());
     }
 }
 
-fn align(x: &[u8], y: &[u8]) {
+fn align(args: &Args, x: &[u8], y: &[u8]) {
     let scoring = get_blosum62(-12, -1);
     let mut aligner = Aligner::with_capacity_and_scoring(x.len(), y.len(), scoring);
-    let alignment = aligner.global(x, y);
-    let stats = score_stats(&alignment, x, y);
-    println!("Identity: {}, Gaps: {}", stats.0, stats.1);
-    //println!("{:#?}", alignment);
+    let alignment = if args.local {
+        aligner.local(x, y)
+    } else if args.semi_global {
+        aligner.semiglobal(x, y)
+    } else {
+        aligner.global(x, y)
+    };
+    show_alignment(&alignment, x, y);
+}
+
+fn show_alignment(alignment: &Alignment, sequence_x: &[u8], sequence_y: &[u8]) {
+    let stats = score_stats(alignment, sequence_x, sequence_y);
+
+    println!(
+        "Identity: {}, Gaps: {}, Score: {}, CIGAR: {}",
+        stats.0,
+        stats.1,
+        alignment.score,
+        alignment.cigar(false)
+    );
+
+    let mut top = Vec::new();
+    let mut bottom = Vec::new();
+    let mut x = alignment.xstart;
+    let mut y = alignment.ystart;
+    for step in &alignment.operations {
+        match step {
+            AlignmentOperation::Del => {
+                top.push(b'-');
+                bottom.push(sequence_y[y]);
+                y += 1;
+            }
+            AlignmentOperation::Ins => {
+                top.push(sequence_x[x]);
+                bottom.push(b'-');
+                x += 1;
+            }
+            AlignmentOperation::Subst => {
+                top.push(sequence_x[x]);
+                bottom.push(sequence_y[y]);
+                x += 1;
+                y += 1;
+            }
+            AlignmentOperation::Match => {
+                top.push(sequence_x[x]);
+                bottom.push(sequence_y[y]);
+                x += 1;
+                y += 1;
+            }
+            AlignmentOperation::Xclip(_) => todo!(),
+            AlignmentOperation::Yclip(_) => todo!(),
+        }
+    }
+    const CHUNK_SIZE: usize = 50;
+    for section in top.chunks(CHUNK_SIZE).zip(bottom.chunks(CHUNK_SIZE)) {
+        println!("{}", String::from_utf8_lossy(section.0));
+        println!("{}\n", String::from_utf8_lossy(section.1));
+    }
 }
 
 pub fn get_blosum62(gap_open: i32, gap_extend: i32) -> Scoring<impl Fn(u8, u8) -> i32> {
@@ -86,7 +156,7 @@ pub fn score_stats(alignment: &Alignment, sequence_x: &[u8], sequence_y: &[u8]) 
     debug_assert!(x == alignment.xend);
     debug_assert!(y == alignment.yend);
     (
-        identical as f64 / (x_len).min(y_len) as f64,
-        (gaps - (x_len as i32 - y_len as i32).abs()) as f64 / x_len.min(y_len) as f64,
+        identical as f64 / (x_len).max(y_len) as f64,
+        gaps as f64 / x_len.max(y_len) as f64,
     )
 }
