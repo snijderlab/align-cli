9f69de5a9bcfabd033e59f1b374d3f14cb3480fc
Douwe Schulte <d.schulte@uu.nl>
2023-11-08 11:18:03 +0100

    Allowed custom placement rules and N/C terminal isobaric generation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use bio::alignment::pairwise::{Aligner, Scoring};
 use clap::{Args, Parser};
 use colored::Colorize;
 use rustyms::{
-    error::CustomError,
     find_isobaric_sets,
     modification::{GnoComposition, ReturnModification},
     ontologies::*,
@@ -56,11 +55,16 @@ struct Cli {
     #[arg(short, long, default_value_t = IsobaricNumber::Limited(25), value_parser=options_parse)]
     isobaric: IsobaricNumber,
 
-    /// All possible fixed modifications that will be used in the isobaric sets generation, separated by commas `,`
+    /// All possible fixed modifications that will be used in the isobaric sets generation, separated by commas `,`.
+    /// You can overwrite the default placement rules in the same was as for variable modifications.
     #[arg(short = 'F', long, default_value_t = Modifications::None, value_parser=modifications_parse, allow_hyphen_values=true)]
     fixed: Modifications,
 
-    /// All possible variable modifications that will be used in the isobaric sets generation, separated by commas `,`
+    /// All possible variable modifications that will be used in the isobaric sets generation, separated by commas `,`. 
+    /// You can overwrite the default placement rules in the following way: `@AA-pos` where `AA` is the list of all amino acids it can be 
+    /// placed or a star to indicate it can be placed on all locations, and `pos` is the position: * -> Anywhere, 
+    /// N/n -> N terminal (protein/peptide), C/c -> C terminal (protein/peptide). The position can be left out which defaults to Anywhere.
+    /// Examples for the rules: `Carboxymethyl@C`, `Oxidation@WFH`, `Amidated@*-C`.
     #[arg(short, long, default_value_t = Modifications::None, value_parser=modifications_parse, allow_hyphen_values=true)]
     variable: Modifications,
 
@@ -137,10 +141,10 @@ fn options_parse(input: &str) -> Result<IsobaricNumber, &'static str> {
 #[derive(Debug, Clone)]
 enum Modifications {
     None,
-    Some(Vec<Modification>),
+    Some(Vec<(Modification, Option<PlacementRule>)>),
 }
 impl Modifications {
-    fn mods(&self) -> &[Modification] {
+    fn mods(&self) -> &[(Modification, Option<PlacementRule>)] {
         match self {
             Self::None => &[],
             Self::Some(m) => m,
@@ -154,7 +158,7 @@ impl Display for Modifications {
             Self::Some(mods) => {
                 let mut start = true;
                 for m in mods {
-                    write!(f, "{}{}", if start { "" } else { "," }, m).unwrap();
+                    write!(f, "{}{}", if start { "" } else { "," }, m.0).unwrap();
                     start = false;
                 }
                 Ok(())
@@ -162,7 +166,25 @@ impl Display for Modifications {
         }
     }
 }
-fn modifications_parse(input: &str) -> Result<Modifications, &'static str> {
+fn modifications_parse(input: &str) -> Result<Modifications, String> {
+    fn parse_position(pos: &str) -> Result<Position, String> {
+        match pos {
+            "*" => Ok(Position::Anywhere),
+            "C" => Ok(Position::ProteinCTerm),
+            "c" => Ok(Position::AnyCTerm),
+            "N" => Ok(Position::ProteinNTerm),
+            "n" => Ok(Position::AnyNTerm),
+            _ => Err(format!("'{pos}' is not a valid modification placement position use any of: */N/n/C/c"))
+        }
+    }
+    fn parse_aa(aa: &str) -> Result<Option<Vec<AminoAcid>>, String> {
+        if aa == "*" {
+            Ok(None)
+        } else {
+            Ok(Some(aa.chars().map(|c| AminoAcid::try_from(c).map_err(|_| format!("'{c}' is not a valid amino acid"))).collect::<Result<Vec<_>,_>>()?))
+        }
+    }
+
     if input.is_empty() {
         Ok(Modifications::None)
     } else {
@@ -170,19 +192,35 @@ fn modifications_parse(input: &str) -> Result<Modifications, &'static str> {
             .trim_end_matches(',')
             .split(',')
             .map(|m| {
-                Modification::try_from(m, 0..m.len(), &mut Vec::new()).map(|m| match m {
-                ReturnModification::Defined(d) => d,
-                _ => {
-                    panic!("Can not define ambiguous modifications for the modifications parameter")
+                if let Some((head, tail)) = m.split_once('@') {
+                    let modification = 
+                    Modification::try_from(head, 0..head.len(), &mut Vec::new()).map_err(|e| e.to_string()).and_then(|m| if let Some(d) = m.defined() {
+                        Ok(d) } else {
+                            Err("Can not define ambiguous modifications for the modifications parameter".to_string())
+                        }
+                    )?;
+                    let rule = if let Some((aa, position)) = tail.split_once('-') {
+                        if let Some(aa) = parse_aa(aa)? {
+                            PlacementRule::AminoAcid(aa, parse_position(position)?)
+                        } else {
+                            PlacementRule::Terminal(parse_position(position)?)
+                        }
+                    } else if let Some(aa) = parse_aa(tail)? {
+                            PlacementRule::AminoAcid(aa, Position::Anywhere)
+                        } else {
+                            return Err("Cannot have a modification rule that allows a modification on all position on all amino acids".to_string())
+                        };
+                    Ok((modification, Some(rule)))
+                } else {
+                    Modification::try_from(m, 0..m.len(), &mut Vec::new()).map_err(|e| e.to_string()).and_then(|m| if let Some(d) = m.defined() {
+                        Ok((d, None)) } else {
+                            Err("Can not define ambiguous modifications for the modifications parameter".to_string())
+                        }
+                    )
                 }
-            })
-            })
-            .collect::<Result<Vec<Modification>, CustomError>>()
+                })
+            .collect::<Result<Vec<(Modification, Option<PlacementRule>)>, String>>()
             .map(Modifications::Some)
-            .map_err(|e| {
-                print!("{e}");
-                "Failed to parse modification"
-            })
     }
 }
 
